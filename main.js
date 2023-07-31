cf50c2f98a758750102f62c465f8d666f0f9ab3e
Bonolo <bonololloyd003@gmail.com>
2023-07-31 03:29:52 +0200

    adding new files

---
+++ main.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".roles",{
+  strings:["Software Developer.","Email Developer.","Shopify Developer."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
+
+const menuIcon = document.querySelector('.menu-icon');
+
+menuIcon.addEventListener('click',function(){
+  const navbar = document.querySelector('.navbar');
+  menuIcon.classList.toggle('active')
+  navbar.classList.toggle('active')
+})
+
+ const navlinks = document.querySelectorAll('.navLinks')
+function active(){
+  navlinks.forEach((i) =>
+  i.classList.remove('active'));
+  this.classList.add('active');
+}
+
+navlinks.forEach((i) =>
+i.addEventListener('click',active))
