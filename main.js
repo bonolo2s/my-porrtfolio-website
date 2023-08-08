94d45801b660dfc3f18703ca723ca058b14be5ca
Bonolo <bonololloyd003@gmail.com>
2023-08-08 15:14:35 +0200

    modifying my roles on main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".roles",{
-  strings:["Software Developer.","Email Developer.","Shopify Developer."],
+  strings:["Software Developer.","Email Developer."],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
